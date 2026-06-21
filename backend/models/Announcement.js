d5b3a84f30993af011d0b26043fd83db5823e525
jackmacha914-hash <jackmacha914@gmail.com>
2026-06-21 18:59:54 +0300

    Announcement.js

--- backend/models/Announcement.js
+++ backend/models/Announcement.js
@@ -1,18 +1,26 @@
 const mongoose = require('mongoose');
 
 const announcementSchema = new mongoose.Schema({
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'School',
+    required: true
+  },
+
   text: {
     type: String,
     required: true,
   },
+
   createdBy: {
     type: String,
     default: 'Unknown',
   },
+
   createdAt: {
     type: Date,
     default: Date.now,
   }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Announcement', announcementSchema);
