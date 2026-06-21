ce1307cf0ecd723c5694a37e0811ed87c412eb16
jackmacha914-hash <jackmacha914@gmail.com>
2026-06-21 19:17:08 +0300

    Book.js

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -2,13 +2,28 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'School',
+    required: true
+  },
+
   title: { type: String, required: true },
   author: { type: String, required: true },
   year: { type: Number },
   genre: { type: String, required: true },
   className: { type: String, required: true },
-  status: { type: String, default: 'available' },
-  available: { type: Number, default: 1, min: 0 }
-});
+
+  status: {
+    type: String,
+    default: 'available'
+  },
+
+  available: {
+    type: Number,
+    default: 1,
+    min: 0
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.models.Book || mongoose.model('Book', bookSchema);
