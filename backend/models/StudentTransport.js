73654d79057516181ed199880af1d779675b7386
jackmacha914-hash <jackmacha914@gmail.com>
2026-06-24 10:03:02 +0300

    StudentTransport.js

--- backend/models/StudentTransport.js
+++ backend/models/StudentTransport.js
@@ -1,8 +1,49 @@
-// models/StudentTransport.js
 const mongoose = require('mongoose');
+
 const StudentTransportSchema = new mongoose.Schema({
-  studentId: { type: String, required: true },
-  busId: { type: mongoose.Schema.Types.ObjectId, ref: 'Bus' },
-  routeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Route' }
+
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'School',
+    required: true,
+    index: true
+  },
+
+  studentId: {
+    type: String,
+    required: true
+  },
+
+  busId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Bus'
+  },
+
+  routeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Route'
+  }
+
+}, {
+  timestamps: true
 });
-module.exports = mongoose.model('StudentTransport', StudentTransportSchema);
+
+// Useful indexes
+StudentTransportSchema.index({
+  school: 1,
+  studentId: 1
+});
+
+StudentTransportSchema.index({
+  school: 1,
+  busId: 1
+});
+
+StudentTransportSchema.index({
+  school: 1,
+  routeId: 1
+});
+
+module.exports =
+  mongoose.models.StudentTransport ||
+  mongoose.model('StudentTransport', StudentTransportSchema);
