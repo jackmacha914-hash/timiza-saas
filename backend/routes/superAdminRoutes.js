81d8a742abc6e88b0b6d1c4589ee5fd16f0c67fd
jackmacha914-hash <jackmacha914@gmail.com>
2026-06-26 21:24:02 +0300

    superAdminRoutes.js

--- backend/routes/superAdminRoutes.js
+++ backend/routes/superAdminRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const superAdminController = require("../controllers/superAdminController");
 
+router.get("/create-school", (req, res) => {
+    res.send("Super Admin route is working!");
+});
+
 // Later we'll protect this with superAdmin middleware
 router.post(
     "/create-school",
