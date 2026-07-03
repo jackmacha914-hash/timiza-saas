a8ef486a01befab8476c8cf781eb323a5d8ae7a7
jackmacha914-hash <jackmacha914@gmail.com>
2026-07-03 10:11:04 +0300

    server.js
    Change CORS origin to allow all domains.

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 // Global CORS – allow frontend domain
 // -------------------------
 app.use(cors({
-  origin: "https://luckyjuniorschool.onrender.com",
+  origin: "",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
