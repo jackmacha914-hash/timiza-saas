d01afecfece49f7e5730d4751391448acbbd8eb0
jackmacha914-hash <jackmacha914@gmail.com>
2026-07-30 06:08:54 +0300

    academicRoutes.js

---
+++ backend/routes/academicRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    authenticateUser,
+    authorizeRoles
+} = require("../middleware/authMiddleware");
+
+const {
+    getDashboard
+} = require("../controllers/academicController");
+
+router.use(authenticateUser);
+
+router.get(
+    "/dashboard",
+    authorizeRoles("admin"),
+    getDashboard
+);
+
+module.exports = router;
