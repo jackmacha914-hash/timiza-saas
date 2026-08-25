142c9875c9b392f8caa1b9a1451a7087401b4ca5
jackmacha914-hash <jackmacha914@gmail.com>
2026-08-25 13:01:49 +0300

    managementUsersRoutes.js

---
+++ backend/routes/managementUsersRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getManagementUsers,
+  createManagementUser,
+  activateManagementUser,
+  suspendManagementUser,
+  deleteManagementUser
+} = require('../controllers/managementUsersController');
+
+
+// Get students + teachers
+router.get('/', getManagementUsers);
+
+
+// Create student / teacher
+router.post('/', createManagementUser);
+
+
+// Activate
+router.patch('/:id/activate', activateManagementUser);
+
+
+// Suspend
+router.patch('/:id/suspend', suspendManagementUser);
+
+
+// Delete
+router.delete('/:id', deleteManagementUser);
+
+
+module.exports = router;
